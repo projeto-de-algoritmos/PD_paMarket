e3f569d9074aa05e41592bdf8fa45cd4ab86a753
Fellipe Araujo <araujo.fellipe@hotmail.com>
2021-04-30 21:29:02 -0300

    Create initial home page.
    Co-authored-by: Ithalo Azevedo <ithaloazevedoo@gmail.com>

--- pa-market/src/pages/Home/index.js
+++ pa-market/src/pages/Home/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import './styles.css';
+import { Link } from 'react-router-dom';
+import Header from '../../components/Header';
+import LogoShop from '../../assets/logo-shop.svg';
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="home-container">
+      <Header />
+      <div className="home-content">
+        <img className="home-image" src={LogoShop} alt="LogoShop" />
+
+        <section className="home-instructions">
+          <Link className="home-button" to="/products">
+            <h1 className="home-button-title">Ver produtos</h1>
+          </Link>
+        </section>
+      </div>
     </div>
   );
 };
