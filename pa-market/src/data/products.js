584e1108f37d92a5d2949d0db90246295f1af67c
Fellipe Araujo <araujo.fellipe@hotmail.com>
2021-05-01 00:20:07 -0300

    Add products data.
    Co-authored-by: Ithalo Azevedo <ithaloazevedoo@gmail.com>

---
+++ pa-market/src/data/products.js
@@ -0,0 +1,52 @@
+export const products = [
+  {
+    id: 1,
+    name: '',
+    price: 200
+  },
+  {
+    id: 2,
+    name: '',
+    price: 200
+  },
+  {
+    id: 3,
+    name: '',
+    price: 200
+  },
+  {
+    id: 4,
+    name: '',
+    price: 200
+  },
+  {
+    id: 5,
+    name: '',
+    price: 200
+  },
+  {
+    id: 6,
+    name: '',
+    price: 200
+  },
+  {
+    id: 7,
+    name: '',
+    price: 200
+  },
+  {
+    id: 8,
+    name: '',
+    price: 200
+  },
+  {
+    id: 9,
+    name: '',
+    price: 200
+  },
+  {
+    id: 10,
+    name: '',
+    price: 200
+  },
+];
