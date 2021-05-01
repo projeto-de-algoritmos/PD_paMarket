6c70ad59cc260b55e186afda22268c30c4f016b8
Fellipe Araujo <araujo.fellipe@hotmail.com>
2021-04-30 21:30:21 -0300

    Configure system routes.
    Co-authored-by: Ithalo Azevedo <ithaloazevedoo@gmail.com>

--- pa-market/src/Routes.js
+++ pa-market/src/Routes.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Products from './pages/Products';
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
-      <Route path="/" component={Home} />
+      <Route path="/" exact component={Home} />
+      <Route path="/products" component={Products} />
     </Switch>
   </BrowserRouter>
 );
