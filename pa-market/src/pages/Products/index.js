0e27b7bef4a9963a47359c6f1e88195d89ccba63
Fellipe Araujo <araujo.fellipe@hotmail.com>
2021-04-30 21:29:34 -0300

    Create initial products page.
    Co-authored-by: Ithalo Azevedo <ithaloazevedoo@gmail.com>

---
+++ pa-market/src/pages/Products/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.css';
+import Header from '../../components/Header';
+
+const Products = () => {
+  return (
+    <div className="products-container">
+      <Header />
+      <h1>Products Page</h1>
+    </div>
+  );
+};
+
+export default Products;
