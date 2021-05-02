47d48ae9d45bc78225bf712f9e830755302f3fa8
Ithalo Azevedo <ithaloazevedoo@gmail.com>
2021-05-02 14:42:06 -0300

    Populate product.js
    Co-Authored-By: Fellipe Araujo <46676030+fellipe-araujo@users.noreply.github.com>

--- pa-market/src/data/products.js
+++ pa-market/src/data/products.js
@@ -1,52 +1,92 @@
 export const products = [
   {
     id: 1,
-    name: '',
-    price: 200
+    name: 'Morango',
+    price: 10,
+    image: '1.png',
   },
   {
     id: 2,
-    name: '',
-    price: 200
+    name: 'Abacate',
+    price: 15,
+    image: '2.png'
   },
   {
     id: 3,
-    name: '',
-    price: 200
+    name: 'Carne bovina',
+    price: 50,
+    image: '3.png'
   },
   {
     id: 4,
-    name: '',
-    price: 200
+    name: 'Frango',
+    price: 30,
+    image: '4.png'
   },
   {
     id: 5,
-    name: '',
-    price: 200
+    name: 'Coca-cola',
+    price: 7,
+    image: '5.png'
   },
   {
     id: 6,
-    name: '',
-    price: 200
+    name: 'Peixe',
+    price: 40,
+    image: '6.png'
   },
   {
     id: 7,
-    name: '',
-    price: 200
+    name: 'Macarrão',
+    price: 12,
+    image: '7.png'
+
   },
   {
     id: 8,
-    name: '',
-    price: 200
+    name: 'Eggs',
+    price: 8,
+    image: '8.png'
   },
   {
     id: 9,
-    name: '',
-    price: 200
+    name: 'Manteiga de amendoim',
+    price: 22,
+    image: '9.png'
   },
   {
     id: 10,
-    name: '',
-    price: 200
+    name: 'Leite',
+    price: 9,
+    image: '10.png'
+  }, {
+    id: 11,
+    name: 'Nutella',
+    price: 20,
+    image: '11.png'
+  },
+  {
+    id: 12,
+    name: 'Kitkat',
+    price: 5,
+    image: '12.png'
+  },
+  {
+    id: 13,
+    name: 'Açaí',
+    price: 25,
+    image: '13.png'
+  },
+  {
+    id: 14,
+    name: 'Pão',
+    price: 8,
+    image: '14.png'
+  },
+  {
+    id: 15,
+    name: 'Apple',
+    price: 1,
+    image: '15.png'
   },
 ];
