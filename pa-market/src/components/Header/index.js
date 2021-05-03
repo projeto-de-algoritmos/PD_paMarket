e5adaf37e593dddd79e8dbbdfd89b8cf852c03b4
Fellipe Araujo <araujo.fellipe@hotmail.com>
2021-05-03 15:33:11 -0300

    Add filter icon in header.
    Co-authored-by: Ithalo Azevedo <ithaloazevedoo@gmail.com>

--- pa-market/src/components/Header/index.js
+++ pa-market/src/components/Header/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './styles.css';
-import { FiShoppingCart } from 'react-icons/fi';
+import { FiShoppingCart, FiFilter } from 'react-icons/fi';
 
-const Header = () => {
+const Header = ({ filterVisible, onClick }) => {
   return (
     <div className="header-container">
-      <FiShoppingCart size={26} color="#FFF" />
-      <h1 className="header-title">paMarket</h1>
+      <div className="header-logo">
+        <FiShoppingCart size={26} color="#FFF" />
+        <h1 className="header-title">paMarket</h1>
+      </div>
+
+      {filterVisible ? (
+        <button className="header-filter" onClick={onClick}>
+          <FiFilter size={26} color="#FFF" />
+        </button>
+      ) : null}
     </div>
   );
 };
