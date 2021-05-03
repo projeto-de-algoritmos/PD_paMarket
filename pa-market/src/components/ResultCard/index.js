21aec02a48b0d02a549cd763b0f4cfd25a9f4783
Fellipe Araujo <araujo.fellipe@hotmail.com>
2021-05-03 15:35:58 -0300

    Update result card and create props for this component.
    Co-authored-by: Ithalo Azevedo <ithaloazevedoo@gmail.com>

--- pa-market/src/components/ResultCard/index.js
+++ pa-market/src/components/ResultCard/index.js
@@ -2,18 +2,27 @@ import React from 'react';
 import './styles.css';
 import { Link } from 'react-router-dom';
 
-const ResultCard = () => {
+const ResultCard = ({ total, value, onChange, onClick }) => {
   return (
     <div className="result-container">
       <div className="result-quantity">
-        <h1>Produtos em estoque:</h1>
-        <h1>XX</h1>
+        <h1 className="result-quantity-title">Produtos em estoque:</h1>
+        <h1 className="result-quantity-value">{total}</h1>
       </div>
 
-      <h1>Digite seu valor:</h1>
-      <input className="result-input" placeholder="R$ 1000" />
+      <h1 className="result-target-value">Digite seu valor:</h1>
+      <input
+        className="result-input"
+        placeholder="R$ 1000"
+        value={value}
+        onChange={onChange}
+      />
 
-      <Link className="result-button" to="/">
+      <Link
+        className={value ? 'result-button-active' : 'result-button-disable'}
+        onClick={onClick}
+        to={value ? '/products/result' : null}
+      >
         <h1 className="result-button-title">Calcular melhor compra</h1>
       </Link>
     </div>
